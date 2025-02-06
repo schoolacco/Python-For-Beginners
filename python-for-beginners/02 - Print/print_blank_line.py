af8e832e186b7b4fb7d739fbc5e9aa29021eb63d
mr-scott32 <128337536+mr-scott32@users.noreply.github.com>
2025-02-06 19:30:14 +1100

    Update print_blank_line.py

--- python-for-beginners/02 - Print/print_blank_line.py
+++ python-for-beginners/02 - Print/print_blank_line.py
@@ -7,7 +7,8 @@ print('Did you see that blank line?')
 
 # '\n' is a special character sequence that means print new line
 # you can use it to break the output over multiple lines
-print('Blank line \nin the middle of string')
+
+print('TESTING')
 
 
 
