661ef7328818685eab7dda0afb7e7138f6f5e2bb
schoolacco <127917983+schoolacco@users.noreply.github.com>
2025-02-07 11:02:52 +1100

    Create test.py

---
+++ more-python-for-beginners/03 - Classes/test.py
@@ -0,0 +1,10 @@
+class test():
+    def input2(var1, var2):
+        var1 = int(input("Num1: "))
+    def __innit__(self, var1, var2):
+        self.var1 = var1
+        if int(var2) == var2:
+           self.var2 = var2/var1
+    def add(self):
+        self = self+1
+        print(self)
